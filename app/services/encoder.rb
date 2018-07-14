d824ff3f492d89a586fe54c0be8c9588f6684ef5
rachel <rachel@joindrover.com>
2018-07-14 17:38:27 +0100

    Crypto Challenge Set 1:2 Fixed XOR

--- app/services/encoder.rb
+++ app/services/encoder.rb
@@ -6,6 +6,9 @@ class Encoder
   BASE_64_CHARACTERS = %w{A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
                           a b c d e f g h i j k l m n o p q r s t u v w x y z
                           0 1 2 3 4 5 6 7 8 9 / +}
+  VOWELS             = %w{a e i o u A E I O U}
+  CONSONANTS         = %w{b c d f g h j k l m n p q r s t v w x y z
+                          B C D F G H J K L M N P Q R S T V W X Y Z}
 
   def get_base_64_index(ch)
     Encoder::BASE_64_CHARACTERS.index(ch)
@@ -55,7 +58,7 @@ class Encoder
   def hex_to_binary(hex_string)
     # extract array of character values from hex string
     # find hex character index, map to 4-bit string and combine
-    bit_string = string_to_array(hex_string).map { |c| to_binary(get_hex_index(c)) }.inject("") { |str, ch| str + ch }
+    string_to_array(hex_string).map { |c| to_binary(get_hex_index(c)) }.inject("") { |str, ch| str + ch }
   end
 
   def binary_to_base_64(binary_string)
@@ -70,7 +73,92 @@ class Encoder
     #encode and combine into one string
     arr2 = arr.map { |s| s.to_i(2) }.map { |i| BASE_64_CHARACTERS[i] }.inject("") { |str, ch| str + ch }
   end
+
+  def hex_to_boolean_array(hex_string)
+    string_to_array(hex_to_binary(hex_string)).map { |b| b == "0" ? false : true }
+  end
+
+  def boolean_array_to_hex_string(boolean_arr)
+    bin_str = boolean_arr.map { |b| b ? "1" : "0" }.inject("") { |str, ch| str + ch }
+
+    # chunk into blocks of 4 and convert back to hex
+    arr = []
+    while (!bin_str.empty?)
+      arr << bin_str.slice!(0..3)
+    end
+
+    arr.map { |bs| bs.to_i(2) }.map { |i| HEX_CHARACTERS[i] }.inject("") { |str, ch| str + ch }
+  end
+
+  def fixed_xor(hex_string_1, hex_string_2)
+    raise "string must be the same length!" unless hex_string_1.size == hex_string_2.size
+
+    boolean_arr_1 = hex_to_boolean_array(hex_string_1)
+    boolean_arr_2 = hex_to_boolean_array(hex_string_2)
+
+    boolean_arr_3 = []
+
+    boolean_arr_1.zip(boolean_arr_2).each do |bit_1, bit_2|
+      boolean_arr_3.push(bit_1 ^ bit_2)
+    end
+
+    boolean_array_to_hex_string(boolean_arr_3)
+  end
+
+  def find_key_to_xor_cipher(hex_string)
+    my_hex = hex_string.dup
+
+    # chunk into bytes (2 hex characters)
+    arr = []
+    while (!my_hex.empty?)
+      arr << my_hex.slice!(0..1)
+    end
+
+    max_percentage = 0
+    winner         = 0.chr
+    answer         = ""
+
+    (0..255).each do |i|
+      char = i.to_s(16) # get hex string of each ascii character
+      hex_arr = arr.map { |h| fixed_xor(h, char) } rescue []       # map fixed-xor with hex of char
+      decoded = hex_array_to_character_string(hex_arr) # decoded the hex_array back into ascii
+      p       = percentage(VOWELS, decoded) + percentage(CONSONANTS, decoded) # compute % of vowels and characters
+      if p > max_percentage   # the highest percentage wins
+        max_percentage = p
+        winner         = i.chr
+        answer         = decoded
+      end
+      puts "#{i}: #{sprintf("%.2f\%", p * 100)} #{decoded}"
+    end
+
+    [max_percentage, winner, answer]
+  end
+
+  def hex_array_to_character_string(hex_arr)
+    hex_arr.map { |h| h.to_i(16).chr(Encoding::ASCII_8BIT) }.inject("") { |str, ch| str + ch }  # decoded the hex_array back into ascii
+  end
+
+  def hex_string_to_character_string(hex_string)
+    my_hex = hex_string.dup
+    # chunk into 2-hex and xor with hex of char
+    hex_arr = []
+    while (!my_hex.empty?)
+      hex_arr << my_hex.slice!(0..1)
+    end
+    hex_array_to_character_string(hex_arr)
+  end
+
+  def percentage(characters, string)
+    string_length = string.size
+    arr           = string_to_array(string)
+    count         = 0
+    arr.each do |ch|
+      count += 1 if characters.include?(ch)
+    end
+    count.to_f / string_length.to_f
+  end
 end
+
 #
 # # str.unpack("C*").map{|i| i.to_s(2).rjust(8, '0')}.inject(""){|str, ch| str + ch}
 # # hex_hash.map{|key, value| value.to_s(2).rjust(4, '0')}.inject(""){|str, ch| str + ch}
--- spec/services/encoder_spec.rb
+++ spec/services/encoder_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe Encoder do
 
-  let(:hex_string) {"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"}
-  let(:base_64_string) {"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"}
-
   context "Crypto Challenge Set 1" do
     it "passes 1:1 hex to base_64 encoding" do
+      hex_string     = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+      base_64_string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
       e = Encoder.new
 
       binary_string = e.hex_to_binary(hex_string)
@@ -14,6 +14,28 @@ RSpec.describe Encoder do
 
       base_64 = e.binary_to_base_64(binary_string)
       expect(base_64).to eq base_64_string
+    end
+
+    it "passes 1:2 fixed xor" do
+      hex_string_1 = "1c0111001f010100061a024b53535009181c"
+      hex_string_2 = "686974207468652062756c6c277320657965"
+      hex_string_3 = "746865206b696420646f6e277420706c6179"
+
+      e = Encoder.new
+
+      expect(e.fixed_xor(hex_string_1, hex_string_2)).to eq hex_string_3
+    end
+    it "passes 1:3 single-byte xor cipher" do
+      hex_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+      # find the character that decodes the cipher which has been xor-ed with it
+
+      e = Encoder.new
+
+      percentage, char, answer = e.find_key_to_xor_cipher(hex_string)
+
+      expect(percentage).to be >= 0.75
+      expect(char).to eq "X"
+      puts answer
     end
   end
 end
